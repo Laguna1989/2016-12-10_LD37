41abc0f618cc7fba95d28f16544636a9fbffcf2c
Thunraz <julian.dinges@gmail.com>
2016-12-10 15:56:42 +0100

    Added a few more room properties

--- source/Rooms/RoomProperties.hx
+++ source/Rooms/RoomProperties.hx
@@ -7,7 +7,14 @@ package;
 class RoomProperties
 {
 	public var AirConditioningLevel : Int = 0;
+	public var TVLevel : Int = 0;
+	public var Windows : Int = 0;
+	public var WiFi : Bool = false;
 	public var BedLevel : Int = 0;
+	public var Plants : Int = 0;
+	public var Minibar : Bool = false;
+	public var BathroomLevel : Int = 0;
+
 	public var NoiseFactor:Float = 0;
 	
 	public function new() 
